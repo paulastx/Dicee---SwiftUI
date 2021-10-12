cc1d003897a0d7ab0fb0c493cf76dfed10de1166
paulastx <paula.stx@gmail.com>
2021-10-12 16:13:11 -0300

    Initial Commit

---
+++ Dice-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice-SwiftUI
+//
+//  Created by Paula Souza Teixeira on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dice-SwiftUI/Dice_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_SwiftUIApp.swift
+//  Dice-SwiftUI
+//
+//  Created by Paula Souza Teixeira on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Dice_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
